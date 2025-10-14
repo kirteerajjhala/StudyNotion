210178c3daef77e1433de7ebc61b4d4edf0d4d6a
Kirteeraj Jhala <kirteerajjhala@gmial.com>
2025-10-14 17:38:31 +0530

    live website login problem solve

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://studynotion-backend-httk.onrender.com";
+const BASE_URL = "https://studynotion-backend-httk.onrender.com/api/v1";
 
 // AUTH ENDPOINTS
 export const endpoints = {
